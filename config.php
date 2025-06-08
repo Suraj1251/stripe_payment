549fdd905c183f1155d7399850fbfd418ffc891a
Suraj Chandra <131673879+Suraj1251@users.noreply.github.com>
2025-06-08 11:59:07 +0530

    uploard

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+
+\Stripe\Stripe::setApiKey('sk_test_51PbPoSRs7vn8q6YUR1y2Vvz9CPzeE8mayNwuF73P0ZB0zvhGh96As8EU5WYLcEXZlFKaPSNU5Vbz2oWckMlAG3gX00B43eGT8M'); // Replace with your actual Secret Key
+?>
